99ec2a3ea7a577149add9fd1e3c0036bda3fff23
Victor Ian <107140045+kbldevelop@users.noreply.github.com>
2023-06-01 17:03:59 -0300

    Funções adicionadas

---
+++ funcao.js
@@ -0,0 +1,20 @@
+let esconde = document.getElementById(`esc`);
+let esconde1 = document.getElementById(`esc1`);
+let mostra = document.getElementById(`mst`);
+
+let escondeBotao = document.getElementById(`mostra1`);
+
+
+esconde.style.display = `none`;
+esconde1.style.display = `none`;
+
+function mostrar() {
+    esconde.style.display = `block`;
+    esconde1.style.display = `block`;
+
+    escondeBotao.style.display = `none`
+}
+
+
+
+
